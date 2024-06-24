90f3555ba5774a68473a0044347fca82aeb2abcf
markozimic <mzimic04@gmail.com>
2024-06-24 16:51:34 -0400

    added z

--- start.py
+++ start.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(1, 10, 100)
-y = 2 * x**2
+z = np.linspace(0, 1, 100)
+y = 2 * x**2 + 30 * z
 
 plt.figure()
 plt.plot(x, y, 'b.')
